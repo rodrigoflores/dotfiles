cc9fa73209742ae43fa109646e20ee16941e3dc4
Rodrigo Flores <mail@rodrigoflores.org>
2013-10-25 22:29:19 -0200

    Added git ps1

--- zshrcs/prompt.zsh
+++ zshrcs/prompt.zsh
@@ -4,7 +4,7 @@ source ~/.git-prompt.sh
 
 export GIT_PS1_SHOWDIRTYSTATE=1
 export GIT_PS1_SHOWUNTRACKEDFILES=1
-export GIT_PS1_SHOWUPSTREAM=1
+export GIT_PS1_SHOWUPSTREAM="auto"
 export GIT_PS1_SHOWCOLORHINTS=1
 
 RUBY_VERSION=$(rbenv version-name)

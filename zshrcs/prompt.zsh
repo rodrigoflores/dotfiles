7ee684a70bb58e42e376a65f7ad137f908306597
Rodrigo Flores <mail@rodrigoflores.org>
2013-10-28 22:03:49 -0200

    Prompt reverted

--- zshrcs/prompt.zsh
+++ zshrcs/prompt.zsh
@@ -1,25 +1,8 @@
-autoload -U colors && colors
+setopt prompt_subst
 
-source ~/.git-prompt.sh
+local smiley="%(?,%{$fg[green]%}☺%{$reset_color%},%{$fg[red]%}☹%{$reset_color%})"
 
-export GIT_PS1_SHOWDIRTYSTATE=1
-export GIT_PS1_SHOWUNTRACKEDFILES=1
-export GIT_PS1_SHOWUPSTREAM="auto"
-export GIT_PS1_SHOWCOLORHINTS=1
+PROMPT='%~
+${smiley}  %{$reset_color%}'
 
-
-current_commit ()
-{
-  if ! git rev-parse --git-dir > /dev/null 2>&1; then
-    ""
-  else
-    git rev-parse --short HEAD
-  fi
-}
-
-RUBY_VERSION=
-CURRENT_COMMIT=$(current_commit)
-GIT=$(__git_ps1 "[%s]")
-
-RPROMPT="$(rbenv version-name) $(__git_ps1 "[%s]") $(current_commit)"
-PROMPT="%~ %# "
+RPROMPT='%{$fg[white]%} $(rbenv version-name)$(~/bin/git-cwd-info)%{$reset_color%}'

a8203879a7efe95d74f70b29ba89150aa5fb0622
Rodrigo Flores <mail@rodrigoflores.org>
2014-11-23 22:59:12 -0200

    Using git prompt on prompt

--- zshrcs/git.zsh
+++ zshrcs/git.zsh
@@ -1,3 +1,4 @@
 zstyle ':completion:*:*:git:*' script ~/.git-completion.bash
+source ~/.git-prompt.sh
 
 eval "$(hub alias -s)"
--- zshrcs/prompt.zsh
+++ zshrcs/prompt.zsh
@@ -1,9 +1,18 @@
 autoload -U colors && colors
 setopt prompt_subst
 
-local smiley="%(?,%{$fg[green]%}☺%{$reset_color%},%{$fg[red]%}☹%{$reset_color%})"
+local smiley="%(?,%{$fg[green]%}✓%{$reset_color%},%{$fg[red]%}✖%{$reset_color%})"
 
-PROMPT='%~
+PROMPT='%~ $fg[green]${STACK}%{$reset_color%} %T
 ${smiley}  %{$reset_color%}'
 
-RPROMPT='%{$fg[white]%} $(rbenv version-name)$(~/bin/git-cwd-info)%{$reset_color%}'
+RUBY_VERSION=$(rbenv version-name)
+
+GIT_PS1_SHOWDIRTYSTATE=true
+GIT_PS1_SHOWSTASHSTATE=true
+GIT_PS1_SHOWCOLORHINTS=true
+GIT_PS1_SHOWUPSTREAM=true
+
+GIT_REPO_PATH=`git rev-parse --git-dir 2>/dev/null`
+
+RPROMPT='%{$fg[white]%} $(rbenv version-name) $GIT_COMMIT_ID $(__git_ps1 "%s")%{$reset_color%}'

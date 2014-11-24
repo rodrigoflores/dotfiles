2e2c7132c94f638a8d6c34514f74fe275ac42444
Rodrigo Flores <mail@rodrigoflores.org>
2014-11-23 22:59:44 -0200

    No git commit id

--- zshrcs/prompt.zsh
+++ zshrcs/prompt.zsh
@@ -15,4 +15,4 @@ GIT_PS1_SHOWUPSTREAM=true
 
 GIT_REPO_PATH=`git rev-parse --git-dir 2>/dev/null`
 
-RPROMPT='%{$fg[white]%} $(rbenv version-name) $GIT_COMMIT_ID $(__git_ps1 "%s")%{$reset_color%}'
+RPROMPT='%{$fg[white]%} $(rbenv version-name) $(__git_ps1 "%s")%{$reset_color%}'

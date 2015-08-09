42b95b932a463074804c2590e6a1298df1ff5758
Rodrigo Flores <mail@rodrigoflores.org>
2015-08-08 21:19:51 -0300

    Install.sh using a better name

--- install.sh
+++ install.sh
@@ -1,17 +1,19 @@
+#!/bin/sh
+
 echo "Starting to copy dotfiles"
 
 DOTFILES_DIR="~/.dotfiles" 
 echo "Dotfiles directory: $DOTFILES_DIR"
 
-copyDotFile (){
+copy_dot_file () {
     echo "Linking $DOTFILES_DIR/$1 to $2"
     ln -svi $DOTFILES_DIR/$1 $2
 }
 
-copyDotFile ssh_config ~/.ssh/config
-copyDotFile gemrc ~/.gemrc
-copyDotFile railsrc ~/.railsrc
-copyDotFile zshrc ~/.zshrc
-copyDotFile pryrc ~/.pryrc
-copyDotFile gitconfig ~/.gitconfig
-copyDotFile gitignore ~/.gitignore 
+copy_dot_file ssh_config ~/.ssh/config
+copy_dot_file gemrc ~/.gemrc
+copy_dot_file railsrc ~/.railsrc
+copy_dot_file zshrc ~/.zshrc
+copy_dot_file pryrc ~/.pryrc
+copy_dot_file gitconfig ~/.gitconfig
+copy_dot_file gitignore ~/.gitignore

96544afb233a32333886d51f38f881d02089d60c
Rodrigo Flores <mail@rodrigoflores.org>
2015-08-15 13:33:31 -0300

    Added lein profiles on .dot-files

---
+++ lein/profiles.clj
@@ -0,0 +1,8 @@
+{:user
+ {:dependencies [[cljdev "0.3.0-SNAPSHOT"]
+                 [org.clojure/tools.nrepl "0.2.10"]]
+  :plugins [[cider/cider-nrepl "0.9.1"]
+            [org.clojure/tools.nrepl "0.2.10"]
+            [lein-ancient "0.6.7"]
+            [lein-midje "3.0.0"]]
+  :injections   [(require 'nu)]}}

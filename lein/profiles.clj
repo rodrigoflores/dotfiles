d3c8edf678da96f59159d41584057edef336430a
Rodrigo Flores <mail@rodrigoflores.org>
2016-02-21 09:15:21 -0300

    Added new things to lein profiles

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -3,8 +3,10 @@
                  [org.clojure/tools.nrepl "0.2.12"]]
   :plugins [[cider/cider-nrepl "0.11.0-SNAPSHOT"]
             [org.clojure/tools.nrepl "0.2.10"]
-            [app-with-midje/lein-template "0.2.0-SNAPSHOT"]
+            [app-with-midje/lein-template "0.1.0"]
             [refactor-nrepl "2.0.0-SNAPSHOT"]
             [lein-ancient "0.6.7"]
-            [lein-midje "3.0.0"]]
-  :injections   [(require 'nu)]}}
+            [lein-midje "3.2"]
+            [venantius/ultra "0.4.0"]
+            [venantius/yagni "0.1.4"]]
+  :injections [(require 'nu)]}}

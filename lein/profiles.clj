2e352ddcd7d0f694334e8a573769b7307914ba31
Rodrigo Flores <rlmflores@fastmail.com>
2016-05-26 16:32:10 -0300

    Lein versions using melpa stable

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,10 +1,10 @@
 {:user
  {:dependencies [[cljdev "0.3.0-SNAPSHOT"]
                  [org.clojure/tools.nrepl "0.2.12"]]
-  :plugins [[cider/cider-nrepl "0.12.0-SNAPSHOT"]
+  :plugins [[cider/cider-nrepl "0.12.0"]
             [org.clojure/tools.nrepl "0.2.10"]
             [app-with-midje/lein-template "0.1.0"]
-            [refactor-nrepl "2.3.0-SNAPSHOT"]
+            [refactor-nrepl "2.2.0"]
             [lein-ancient "0.6.7"]
             [lein-midje "3.2"]
             [venantius/ultra "0.4.0"]

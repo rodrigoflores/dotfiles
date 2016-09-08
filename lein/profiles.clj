d00c22098ee7af661e3beb55ea1f182c19af9849
Rodrigo Flores <rlmflores@fastmail.com>
2016-09-08 08:04:30 -0300

    Updating nrepl

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,7 +1,7 @@
 {:user
  {:dependencies [[cljdev "0.3.0-SNAPSHOT"]
                  [org.clojure/tools.nrepl "0.2.12"]]
-  :plugins [[cider/cider-nrepl "0.12.0"]
+  :plugins [[cider/cider-nrepl "0.13.0"]
             [org.clojure/tools.nrepl "0.2.10"]
             [app-with-midje/lein-template "0.1.0"]
             [refactor-nrepl "2.2.0"]

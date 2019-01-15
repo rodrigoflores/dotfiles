ae1443cb184ee4fccbe88f3d69857d46654c8b85
Rodrigo Flores <rlmflores@fastmail.com>
2019-01-15 12:47:02 -0200

    Fixed refactor-nrepl

--- fish/conf.d/env_vars.fish
+++ fish/conf.d/env_vars.fish
@@ -1,1 +1,1 @@
-set -gx autojump_fish /usr/share/autojump/autojump.fish
+
--- lein/profiles.clj
+++ lein/profiles.clj
@@ -8,11 +8,12 @@
             [lein-figwheel "0.5.6"]
             [lein-ancient "0.6.7"]
             [lein-midje "3.2"]
+            [refactor-nrepl "2.4.0"]
             [venantius/yagni "0.1.4"]
             [lein-gorilla "0.3.6"]]
   :injections [(require 'nu)]}
  :middleware [[cider/cider-nrepl "0.20.0"]
-              [refactor-nrepl "2.4.0-SNAPSHOT"]]
+              [refactor-nrepl "2.4.0"]]
  :repl {:dependencies [[org.clojure/tools.nrepl "0.2.13"]
                        [nrepl/nrepl "0.5.3"]
                        [com.clojure-goes-fast/clj-memory-meter "0.1.2"]]}}

85df405c510469828b409fcbb0b88a12a1db76ad
Rodrigo Flores <rlmflores@fastmail.com>
2019-03-06 15:17:24 -0300

    Update profiles

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -4,7 +4,7 @@
                  [com.clojure-goes-fast/clj-memory-meter "0.1.2"]]
   :signing {:gpg-key "C692C2C1"}
   :plugins [[app-with-midje/lein-template "0.4.0"]
-            [cider/cider-nrepl "0.20.0"]
+            [cider/cider-nrepl "0.21.0"]
             [lein-figwheel "0.5.6"]
             [lein-ancient "0.6.7"]
             [lein-midje "3.2"]
@@ -12,8 +12,8 @@
             [venantius/yagni "0.1.4"]
             [lein-gorilla "0.3.6"]]
   :injections [(require 'nu)]}
- :middleware [[cider/cider-nrepl "0.20.0"]
+ :middleware [[cider/cider-nrepl "0.21.1"]
               [refactor-nrepl "2.4.0"]]
  :repl {:dependencies [[org.clojure/tools.nrepl "0.2.13"]
-                       [nrepl/nrepl "0.5.3"]
+                       [nrepl/nrepl "0.6.0"]
                        [com.clojure-goes-fast/clj-memory-meter "0.1.2"]]}}

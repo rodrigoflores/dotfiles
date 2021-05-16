4a29db12bb0ee920db1cadcb1a0b9c13eae596a0
Rodrigo Flores <rodrigo.flores@nubank.com.br>
2021-05-16 10:14:13 -0300

    Interactive doc

--- fish/config.fish
+++ fish/config.fish
@@ -4,7 +4,7 @@ source $HOME/.dotfiles/fish/secret.fish
 source $HOME/.dotfiles/fish/pyenv.fish
 source $HOME/.dotfiles/fish/rbenv.fish
 
-
+# https://unix.stackexchange.com/questions/278649/how-can-i-check-if-a-shell-is-login-interactive-batch-in-fish
 if status --is-interactive
    set fish_greeting
 

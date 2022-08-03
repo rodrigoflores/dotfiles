40ad76dda42c31421dd6cd6b492c9cb967feee44
Rodrigo Flores <flores@hey.com>
2022-08-03 11:32:31 -0300

    Homebrew as first on path

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,5 @@
-source $HOME/.dotfiles/fish/alias.fish
 source $HOME/.dotfiles/fish/env_vars.fish
+source $HOME/.dotfiles/fish/alias.fish
 source $HOME/.dotfiles/fish/secret.fish
 source $HOME/.dotfiles/fish/pyenv.fish
 source $HOME/.dotfiles/fish/rbenv.fish
--- fish/env_vars.fish
+++ fish/env_vars.fish
@@ -3,18 +3,20 @@ set -xg NU_HOME $CODE_HOME/nu
 set -xg NUDEV_HOME $NU_HOME/nudev
 set -xg NUCLI_HOME $NU_HOME/nucli
 
-fish_add_path $NUCLI_HOME
 fish_add_path /usr/local/bin
-fish_add_path /usr/bin
-fish_add_path /bin
+fish_add_path -p $NUCLI_HOME
+fish_add_path -a /usr/bin
+fish_add_path -a /bin
+fish_add_path -p /usr/local/bin
 fish_add_path /usr/sbin
 fish_add_path /sbin
-fish_add_path $NUDEV_HOME $PATH
+fish_add_path $NUDEV_HOME
 fish_add_path "/Applications/Emacs.app/Contents/MacOS/bin-x86_64-10_14"
 fish_add_path $HOME/.rbenv/bin
 fish_add_path /Applications/Emacs.app/Contents/MacOS
 fish_add_path $HOME/.cargo/bin
-fish_add_path /opt/homebrew/bin
+fish_add_path -m /opt/homebrew/bin
+fish_add_path -m /usr/local/bin
 
 set -x ARTIFACTS_S3_BUCKET nu-artifacts
 set -x ARTIFACTS_AWS_REGION sa-east-1

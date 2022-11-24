a3663480f16100cc8b48252d66a9d1773469518b
Rodrigo Flores <rodrigo.flores@nubank.com.br>
2022-11-24 17:38:52 -0300

    Updates

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,25 +1,27 @@
 {:user
  {:dependencies [[org.clojure/tools.nrepl "0.2.13"]
                  [com.clojure-goes-fast/clj-memory-meter "0.1.2"]
-                 [cider/cider-nrepl "0.28.4"]
+                 [cider/cider-nrepl "0.28.5"]
                  [profit "0.1.0-SNAPSHOT"]]
   :signing {:gpg-key "C692C2C1"}
   :plugins [[app-with-midje/lein-template "0.4.0"]
             [lein-figwheel "0.5.6"]
             [lein-ancient "0.6.7"]
             [lein-midje "3.2"]
-            [refactor-nrepl "3.5.2"]
+            [refactor-nrepl "3.5.5"]
             [venantius/yagni "0.1.4"]
             [refactor-nrepl "3.5.4"]
             [lein-gorilla "0.3.6"]
-            [cider/cider-nrepl "0.28.4"]
-            [com.jakemccrary/lein-test-refresh "0.24.1"]]
+;;            [cider/cider-nrepl "0.28.4"]
+            [cider/cider-nrepl "0.28.5"]
+            [com.jakemccrary/lein-test-refresh "0.24.1"]
+            [cheshire "5.11.0"]]
   :injections [(require '$)
                (#'clojure.core/load-data-readers)
                (set! *data-readers* (.getRawRoot #'*data-readers*))
                ($/set-generic-tags!)]}
  :repl {:dependencies [[org.clojure/tools.nrepl "0.2.13"]
-                       [cider/cider-nrepl "0.28.4"]
+                       [cider/cider-nrepl "0.28.5"]
                        [mx.cider/enrich-classpath "1.9.0"]
                        [nrepl/nrepl "0.9.0"]
                        [com.clojure-goes-fast/clj-memory-meter "0.1.2"]]}}

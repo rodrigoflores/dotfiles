40ed35c27bb944ab4de9f53524829edfd5a48bac
Rodrigo Flores <flores@hey.com>
2023-12-27 20:00:04 -0300

    :construction: Adding first fish files

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,25 @@
+source ./env_vars.fish
+source ./secret.fish
+# source $HOME/.dotfiles/fish/alias.fish
+# source $HOME/.dotfiles/fish/secret.fish
+# source $HOME/.dotfiles/fish/pyenv.fish
+# source $HOME/.dotfiles/fish/rbenv.fish
+# source $HOME/.dotfiles/fish/jenv.fish
+# source $HOME/.dotfiles/fish/zoxide.fish
+
+# set -U SXHKD_SHELL /usr/bin/bash
+
+# starship init fish | source
+
+# set --erase fish_greeting
+
+# source "$HOME/.dotfiles/fish/fish_greeting.fish"
+
+echo "loading new file"
+
+# https://unix.stackexchange.com/questions/278649/how-can-i-check-if-a-shell-is-login-interactive-batch-in-fish
+# if status --is-interactive
+#    neofetch
+
+#    fortune $HOME/.dotfiles/fortune/books
+#  end

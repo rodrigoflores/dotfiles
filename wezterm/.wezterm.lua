25922270b8474c812d4d53e366c679525ffd1ef2
Rodrigo Flores <flores@hey.com>
2024-08-17 09:23:26 -0300

    :wrench: Changed theme

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -8,11 +8,9 @@ local config = wezterm.config_builder()
 config.font = wezterm.font 'BerkeleyMono Nerd Font'
 -- config.font = wezterm.font({ family = 'Berkeley Mono' })
 config.font_size = 17
-config.color_scheme = 'Tokyo Night'
+config.color_scheme = 'Tokyo Night (Gogh)'
 config.default_prog = { '/opt/homebrew/bin/fish', '-l' }
 --config.default_cursor_style = 'BlinkingBlock'
-
-wezterm.log_info("hello world! my name is " .. wezterm.hostname())
 
 wezterm.on('update-status', function(window)
 end)

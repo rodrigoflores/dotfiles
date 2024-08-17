722dc4d248b8ec7ddff49637ecc1108ec5e90a7f
Rodrigo Flores <flores@hey.com>
2024-08-17 07:58:04 -0300

    :sparkles: Wezterm changes

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -5,10 +5,17 @@ local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
 -- This is where you actually apply your config choices
-config.font = wezterm.font 'EnvyCodeR Nerd Font'
-config.color_scheme = 'Dracula'
+config.font = wezterm.font 'BerkeleyMono Nerd Font'
+-- config.font = wezterm.font({ family = 'Berkeley Mono' })
+config.font_size = 17
+config.color_scheme = 'Tokyo Night'
 config.default_prog = { '/opt/homebrew/bin/fish', '-l' }
-config.default_cursor_style = 'BlinkingBlock'
+--config.default_cursor_style = 'BlinkingBlock'
+
+wezterm.log_info("hello world! my name is " .. wezterm.hostname())
+
+wezterm.on('update-status', function(window)
+end)
 
 -- and finally, return the configuration to wezterm
 return config

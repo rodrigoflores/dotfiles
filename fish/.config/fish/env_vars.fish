118db168a2d325809b5fc328b516f96edf49c821
Rodrigo Flores <flores@hey.com>
2024-10-10 22:18:42 -0300

    :recycle: Adjusting path

--- fish/.config/fish/env_vars.fish
+++ fish/.config/fish/env_vars.fish
@@ -3,13 +3,13 @@ fish_add_path /usr/local/bin
 fish_add_path -a /usr/bin
 fish_add_path -a /bin
 fish_add_path -p /usr/local/bin
-fish_add_path /usr/sbin
-fish_add_path /sbin
+fish_add_path -a /usr/sbin
+fish_add_path -a /sbin
 
-fish_add_path "/Applications/Emacs.app/Contents/MacOS/bin-x86_64-10_14"
-fish_add_path $HOME/.rbenv/bin
-fish_add_path /Applications/Emacs.app/Contents/MacOS
-fish_add_path $HOME/.cargo/bin
+fish_add_path -a "/Applications/Emacs.app/Contents/MacOS/bin-x86_64-10_14"
+fish_add_path -a $HOME/.rbenv/bin
+fish_add_path -a /Applications/Emacs.app/Contents/MacOS
+fish_add_path -a $HOME/.cargo/bin
 fish_add_path -m /opt/homebrew/bin
 fish_add_path -m /opt/homebrew/sbin
 fish_add_path -m /usr/local/bin
